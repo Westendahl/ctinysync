4410fbbed29f519dc185538b942512781a1ce568
Damslora <bernt.johan.damslora@nordicsemi.no>
2018-12-05 18:54:31 +0100

    test all estimate

--- main.c
+++ main.c
@@ -11,27 +11,27 @@ int main(){
     d1.t_b = 2;
     d1.t_r = 3;
     tinysync_est_ret_t ret = tinysync_est_etimate(&state, &d1);
-    printf("tiny sync test:%u %f %f\n",ret, state.lineset.ab.a, state.lineset.ba.a);
+    printf("tiny sync test:%u %f %f %f %f\n",ret, state.lineset.ab.a, state.lineset.ba.a, state.lineset.ab.b, state.lineset.ba.b);
     d1.t_o = 4;
     d1.t_b = 5;
     d1.t_r = 6;
     ret = tinysync_est_etimate(&state, &d1);
-    printf("tiny sync test:%u %f %f\n",ret, state.lineset.ab.a, state.lineset.ba.a);
+    printf("tiny sync test:%u %f %f %f %f\n",ret, state.lineset.ab.a, state.lineset.ba.a, state.lineset.ab.b, state.lineset.ba.b);
     d1.t_o = 7;
     d1.t_b = 8;
     d1.t_r = 9;
     ret = tinysync_est_etimate(&state, &d1);
-    printf("tiny sync test:%u %f %f\n",ret, state.lineset.ab.a, state.lineset.ba.a);
+    printf("tiny sync test:%u %f %f %f %f\n",ret, state.lineset.ab.a, state.lineset.ba.a, state.lineset.ab.b, state.lineset.ba.b);
     d1.t_o = 10;
     d1.t_b = 11;
     d1.t_r = 12;
     ret = tinysync_est_etimate(&state, &d1);
-    printf("tiny sync test:%u %f %f\n",ret, state.lineset.ab.a, state.lineset.ba.a);
+    printf("tiny sync test:%u %f %f %f %f\n",ret, state.lineset.ab.a, state.lineset.ba.a, state.lineset.ab.b, state.lineset.ba.b);
     d1.t_o = 13;
     d1.t_b = 14;
     d1.t_r = 15;
     ret = tinysync_est_etimate(&state, &d1);
-    printf("tiny sync test:%u %f %f\n",ret, state.lineset.ab.a, state.lineset.ba.a);
+    printf("tiny sync test:%u %f %f %f %f\n",ret, state.lineset.ab.a, state.lineset.ba.a, state.lineset.ab.b, state.lineset.ba.b);
     
     while(1){}
 
